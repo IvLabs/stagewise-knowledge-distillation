faba452982033e7dc6418f4028085366ff986298
akshay <akshayk.vnit@gmail.com>
2019-09-08 17:06:50 +0530

    Using modified torchvision code for resnet50 type models
    Earlier implementation of resnet50 was actually using just the basic blocks instead of Bottleneck blocks. Not removed earlier implementation yet, need to do that also.

---
+++ code/.ipynb_checkpoints/stagewise_small-checkpoint.py
@@ -0,0 +1,167 @@
+
+import matplotlib.pyplot as plt
+from fastai.vision import *
+import torch
+from torchsummary import summary
+torch.cuda.set_device(0)
+
+class Flatten(nn.Module) :
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+def conv2(ni, nf) : 
+    return conv_layer(ni, nf, stride = 2)
+
+class ResBlock(nn.Module):
+    def __init__(self, nf):
+        super().__init__()
+        self.conv1 = conv_layer(nf,nf)
+
+    def forward(self, x): 
+        return (x + self.conv1(x))
+
+def conv_and_res(ni, nf): 
+    return nn.Sequential(conv2(ni, nf), ResBlock(nf))
+
+def conv_(nf) : 
+    return nn.Sequential(conv_layer(nf, nf), ResBlock(nf))
+
+class SaveFeatures :
+    def __init__(self, m) : 
+        self.handle = m.register_forward_hook(self.hook_fn)
+    def hook_fn(self, m, inp, outp) : 
+        self.features = outp
+    def remove(self) :
+        self.handle.remove()
+    
+for repeated in range(3, 5) : 
+    for stage in range(4) :
+        if repeated == 3 and stage == 0 : 
+            continue
+        torch.manual_seed(repeated)
+        torch.cuda.manual_seed(repeated)
+
+        # stage should be in 0 to 4
+        hyper_params = {
+            "stage": stage,
+            "repeated": repeated,
+            "num_classes": 10,
+            "batch_size": 64,
+            "num_epochs": 100,
+            "learning_rate": 1e-4
+        }
+        
+        path = untar_data(URLs.IMAGENETTE)
+        tfms = get_transforms(do_flip = False)
+        data = ImageDataBunch.from_folder(path, train = 'train', valid = 'val', bs = hyper_params["batch_size"], size = 224, ds_tfms = tfms).normalize(imagenet_stats)
+        
+        learn = cnn_learner(data, models.resnet34, metrics = accuracy)
+        learn = learn.load('unfreeze_imagenet_bs64')
+        learn.freeze()
+        # learn.summary()
+
+        net = nn.Sequential(
+            conv_layer(3, 64, ks = 7, stride = 2, padding = 3),
+            nn.MaxPool2d(3, 2, padding = 1),
+            conv_(64),
+            conv_and_res(64, 128),
+            conv_and_res(128, 256),
+            AdaptiveConcatPool2d(),
+            Flatten(),
+            nn.Linear(2 * 256, 128),
+            nn.Linear(128, hyper_params["num_classes"])
+        )
+
+        net.cpu()
+        # no need to load model for 0th stage training
+        if hyper_params['stage'] == 0 : 
+            filename = '../saved_models/small_stage' + str(hyper_params['stage']) + '/model' + str(hyper_params['repeated']) + '.pt'
+        # separate if conditions for stage 1 and others because of irregular naming convention
+        # in the student model.
+        elif hyper_params['stage'] == 1 : 
+            filename = '../saved_models/small_stage0/model' + str(hyper_params['repeated']) + '.pt'
+            net.load_state_dict(torch.load(filename, map_location = 'cpu'))
+        else : 
+            filename = '../saved_models/small_stage' + str(hyper_params['stage']) + '/model' + str(hyper_params['repeated']) + '.pt'
+            net.load_state_dict(torch.load(filename, map_location = 'cpu'))
+        
+        if torch.cuda.is_available() : 
+            net = net.cuda()
+
+        for name, param in net.named_parameters() : 
+            param.requires_grad = False
+            if name[0] == str(hyper_params['stage'] + 1) and hyper_params['stage'] != 0 :
+                param.requires_grad = True
+            elif name[0] == str(hyper_params['stage']) and hyper_params['stage'] == 0 : 
+                param.requires_grad = True
+
+        # saving outputs of all Basic Blocks
+        mdl = learn.model
+        sf = [SaveFeatures(m) for m in [mdl[0][2], mdl[0][4], mdl[0][5], mdl[0][6]]]
+        sf2 = [SaveFeatures(m) for m in [net[0], net[2], net[3], net[4]]]
+        
+        if hyper_params['stage'] == 0 : 
+            filename = '../saved_models/small_stage' + str(hyper_params['stage']) + '/model' + str(hyper_params['repeated']) + '.pt'
+        else : 
+            filename = '../saved_models/small_stage' + str(hyper_params['stage'] + 1) + '/model' + str(hyper_params['repeated']) + '.pt'
+        optimizer = torch.optim.Adam(net.parameters(), lr = hyper_params["learning_rate"])
+        total_step = len(data.train_ds) // hyper_params["batch_size"]
+        train_loss_list = list()
+        val_loss_list = list()
+        min_val = 100
+        for epoch in range(hyper_params["num_epochs"]):
+            trn = []
+            net.train()
+            for i, (images, labels) in enumerate(data.train_dl) :
+                if torch.cuda.is_available():
+                    images = torch.autograd.Variable(images).cuda().float()
+                    labels = torch.autograd.Variable(labels).cuda()
+                else : 
+                    images = torch.autograd.Variable(images).float()
+                    labels = torch.autograd.Variable(labels)
+
+                y_pred = net(images)
+                y_pred2 = mdl(images)
+
+                loss = F.mse_loss(sf2[hyper_params["stage"]].features, sf[hyper_params["stage"]].features)
+                trn.append(loss.item())
+
+                optimizer.zero_grad()
+                loss.backward()
+        #         torch.nn.utils.clip_grad_value_(net.parameters(), 10)
+                optimizer.step()
+
+                # if i % 50 == 49 :
+                    # print('epoch = ', epoch + 1, ' step = ', i + 1, ' of total steps ', total_step, ' loss = ', round(loss.item(), 6))
+
+            train_loss = (sum(trn) / len(trn))
+            train_loss_list.append(train_loss)
+
+            net.eval()
+            val = []
+            with torch.no_grad() :
+                for i, (images, labels) in enumerate(data.valid_dl) :
+                    if torch.cuda.is_available():
+                        images = torch.autograd.Variable(images).cuda().float()
+                        labels = torch.autograd.Variable(labels).cuda()
+                    else : 
+                        images = torch.autograd.Variable(images).float()
+                        labels = torch.autograd.Variable(labels)
+
+                    # Forward pass
+                    y_pred = net(images)
+                    y_pred2 = mdl(images)
+                    loss = F.mse_loss(sf[hyper_params["stage"]].features, sf2[hyper_params["stage"]].features)
+                    val.append(loss.item())
+
+            val_loss = sum(val) / len(val)
+            val_loss_list.append(val_loss)
+            
+            if (epoch + 1) % 5 == 0 : 
+                print('repetition : ', hyper_params["repeated"], ' | stage : ', hyper_params["stage"])
+                print('epoch : ', epoch + 1, ' / ', hyper_params["num_epochs"], ' | TL : ', round(train_loss, 6), ' | VL : ', round(val_loss, 6))
+
+            if val_loss < min_val :
+                # print('saving model')
+                min_val = val_loss
+                torch.save(net.state_dict(), filename)
--- notebooks/models/.ipynb_checkpoints/resnets-50-checkpoint.py
+++ notebooks/models/.ipynb_checkpoints/resnets-50-checkpoint.py
@@ -1,24 +1,26 @@
 from fastai.vision import *
 import warnings
 
+def res_block(nf, depth=1, dense:bool=False, norm_type:Optional[NormType]=NormType.Batch, **conv_kwargs):
+    "Resnet block of `nf` features. `conv_kwargs` are passed to `conv_layer`."
+    norm2 = norm_type
+    if not dense and (norm_type==NormType.Batch): norm2 = NormType.BatchZero
+    layers = []
+    for i in range(depth+1):
+        layers.append(conv_layer(nf, nf, norm_type=norm2, **conv_kwargs))
+    layers.append(MergeLayer(dense))
+    return SequentialEx(*layers)
+
 def res_block_with_depth(n_in, n_out, n_basic_blocks=1, depth=1, dense:bool=False, norm_type:Optional[NormType]=NormType.Batch, **conv_kwargs):
     "Resnet block of `nf` features. `conv_kwargs` are passed to `conv_layer`."
     norm2 = norm_type
     if not dense and (norm_type==NormType.Batch): norm2 = NormType.BatchZero
     layer1 = [conv_layer(n_in, n_out, norm_type=norm_type, stride=(2, 2), **conv_kwargs)]
     if n_basic_blocks == 0 : return layer1[0]
-    layers = [res_block(n_out, depth=depth) for _ in range(n_basic_blocks)]
-    return SequentialEx(*(layer1 + layers))
+    layers = [res_block(n_in, depth=depth) for _ in range(n_basic_blocks)]
+    return SequentialEx(*(layers+layer1))
 
-def res_block(nf, depth=1, dense:bool=False, norm_type:Optional[NormType]=NormType.Batch, **conv_kwargs):
-    "Resnet block of `nf` features. `conv_kwargs` are passed to `conv_layer`."
-    norm2 = norm_type
-    if not dense and (norm_type==NormType.Batch): norm2 = NormType.BatchZero
-    layers = []
-    for i in range(depth):
-        layers.append(conv_layer(nf, nf, norm_type=norm2, **conv_kwargs))
-    layers.append(MergeLayer(dense))
-    return SequentialEx(*layers)
+
 
 def get_resnet(input_channels=3, out_classes=10, n_basic_blocks=1 ,depth=2, n_blocks=3, fc=True):
     """
