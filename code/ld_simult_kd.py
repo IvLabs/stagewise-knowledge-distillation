17bff9b9516e161051e96613c9937ff51763acae
akshay <akshayk.vnit@gmail.com>
2020-03-12 22:36:34 +0530

    Added less data simultaneous training

--- code/ld_simult_kd.py
+++ code/ld_simult_kd.py
@@ -3,12 +3,14 @@ warnings.filterwarnings("ignore", category=UserWarning, module="torch.nn.functio
 from comet_ml import Experiment
 from fastai.vision import *
 import torch
-from models.custom_resnet import *
-from utils import _get_accuracy
 import argparse
+from torchsummary import summary
+from core import SaveFeatures
+from utils import _get_accuracy
+from models.custom_resnet import *
 torch.cuda.set_device(0)
 
-parser = argparse.ArgumentParser(description = 'Training of ResNet type model using less data approach')
+parser = argparse.ArgumentParser(description = 'Simultaneous training of ResNet type model')
 parser.add_argument('-m', choices = ['resnet10', 'resnet14', 'resnet18', 'resnet20', 'resnet26'], help = 'Give the model name from the choices')
 parser.add_argument('-d', choices = ['imagenette', 'imagewoof', 'cifar10'], help = 'Give the dataset name from the choices')
 parser.add_argument('-p', type = int, help = 'Give percentage of dataset')
@@ -16,73 +18,88 @@ parser.add_argument('-e', type = int, help = 'Give number of epochs for training
 parser.add_argument('-s', type = int, help = 'Give random seed')
 args = parser.parse_args()
 
-sz = 224
-stats = imagenet_stats
-num_epochs = args.e
-batch_size = 64
-# model_name = 'resnet10'
-# dataset = 'imagenette'
-model_name = args.m
-dataset = args.d
-
-if dataset == 'imagenette' : 
-    path = untar_data(URLs.IMAGENETTE)
-elif dataset == 'cifar10' : 
-    path = untar_data(URLs.CIFAR)
-elif dataset == 'imagewoof' : 
-    path = untar_data(URLs.IMAGEWOOF)
-
 torch.manual_seed(args.s)
 torch.cuda.manual_seed(args.s)
 
+val = 'val'
+sz = 224
+stats = imagenet_stats
+batch_size = 64
+# dataset = 'imagenette'
+# model_name = 'resnet26'
+
+if args.d == 'imagenette' : 
+    path = untar_data(URLs.IMAGENETTE)
+elif args.d == 'cifar10' : 
+    path = untar_data(URLs.CIFAR)
+elif args.d == 'imagewoof' : 
+    path = untar_data(URLs.IMAGEWOOF)
+
+val = 'val'
+sz = 224
+stats = imagenet_stats
+
+# stage should be in 0 to 5 (5 for classifier stage)
 hyper_params = {
-    "model": model_name, 
-    "dataset": dataset,
+    "dataset": args.d,
+    "model": args.m,
     "repeated": args.s,
     "num_classes": 10,
     "batch_size": batch_size,
-    "num_epochs": num_epochs,
+    "num_epochs": args.e,
     "learning_rate": 1e-4,
     "perc": args.p
 }
 
 new_path = path/('new' + str(args.p))
-batch_size = 64
 tfms = get_transforms(do_flip=False)
+sz = 224
+stats = imagenet_stats
 
+load_name = hyper_params['dataset']
 if hyper_params['dataset'] == 'cifar10' : 
     sz = 32
     stats = cifar_stats
+    load_name = hyper_params['dataset'][ : -2]
 
-data = ImageDataBunch.from_folder(new_path, train = 'train', valid = 'val', test = 'test', bs = batch_size, size = sz, ds_tfms = tfms).normalize(stats)
-    
-if model_name == 'resnet10' :
+data = ImageDataBunch.from_folder(new_path, train = 'train', valid = 'val', test = 'test', bs = hyper_params["batch_size"], size = sz, ds_tfms = tfms).normalize(stats)
+
+learn = cnn_learner(data, models.resnet34, metrics = accuracy)
+learn = learn.load(os.path.expanduser("~") + '/.fastai/data/' + str(hyper_params['dataset']) + '/models/resnet34_' + load_name + '_bs64')
+learn.freeze()
+
+if hyper_params['model'] == 'resnet10' :
     net = resnet10(pretrained = False, progress = False)
-elif model_name == 'resnet14' : 
+elif hyper_params['model'] == 'resnet14' : 
     net = resnet14(pretrained = False, progress = False)
-elif model_name == 'resnet18' :
+elif hyper_params['model'] == 'resnet18' :
     net = resnet18(pretrained = False, progress = False)
-elif model_name == 'resnet20' :
+elif hyper_params['model'] == 'resnet20' :
     net = resnet20(pretrained = False, progress = False)
-elif model_name == 'resnet26' :
+elif hyper_params['model'] == 'resnet26' :
     net = resnet26(pretrained = False, progress = False)
 
 if torch.cuda.is_available() : 
     net = net.cuda()
 
-project_name = 'ld' + str(hyper_params['perc']) + '-no-teacher'
-experiment = Experiment(api_key="1jNZ1sunRoAoI2TyremCNnYLO", project_name=project_name, workspace="akshaykvnit")
-experiment.log_parameters(hyper_params)
-optimizer = torch.optim.Adam(net.parameters(), lr = hyper_params['learning_rate'])
+# saving outputs of all Basic Blocks
+mdl = learn.model
+# for all 5 feature maps
+sf = [SaveFeatures(m) for m in [mdl[0][2], mdl[0][4], mdl[0][5], mdl[0][6], mdl[0][7]]]
+sf2 = [SaveFeatures(m) for m in [net.relu2, net.layer1, net.layer2, net.layer3, net.layer4]]
+num_fm = 5
 
-os.makedirs('../saved_models/' + str(hyper_params['dataset']) + '/less_data' + str(hyper_params['perc']) + '/' + hyper_params['model'] + '_no_teacher', exist_ok = True)
-savename = '../saved_models/' + str(hyper_params['dataset']) + '/less_data' + str(hyper_params['perc']) + '/' + hyper_params['model'] + '_no_teacher/model' + str(hyper_params['repeated']) + '.pt'
-total_step = len(data.train_ds) // hyper_params['batch_size']
+project_name = 'ld' + str(hyper_params['perc']) + '-simultaneous-kd-' + hyper_params['model'] + '-' + hyper_params['dataset']
+experiment = Experiment(api_key="1jNZ1sunRoAoI2TyremCNnYLO", project_name = project_name, workspace="akshaykvnit")
+experiment.log_parameters(hyper_params)
+optimizer = torch.optim.Adam(net.parameters(), lr = hyper_params["learning_rate"])
+total_step = len(data.train_ds) // hyper_params["batch_size"]
 train_loss_list = list()
 val_loss_list = list()
-val_acc_list = list()
 min_val = 0
-for epoch in range(hyper_params['num_epochs']):
+os.makedirs('../saved_models/' + str(hyper_params['dataset']) + '/less_data' + str(hyper_params['perc']) + '/simultaneous/' + hyper_params['model'] + '_classifier', exist_ok = True)
+savename = '../saved_models/' + str(hyper_params['dataset']) + '/less_data' + str(hyper_params['perc']) + '/simultaneous/' + hyper_params['model'] + '_classifier/model' + str(hyper_params['repeated']) + '.pt'
+for epoch in range(hyper_params["num_epochs"]):
     trn = []
     net.train()
     for i, (images, labels) in enumerate(data.train_dl) :
@@ -94,20 +111,21 @@ for epoch in range(hyper_params['num_epochs']):
             labels = torch.autograd.Variable(labels)
 
         y_pred = net(images)
+        y_pred2 = mdl(images)
         
         loss = F.cross_entropy(y_pred, labels)
-        trn.append(loss.item())
-
+        for k in range(num_fm) : 
+            loss += F.mse_loss(sf[k].features, sf2[k].features)
+        
+        trn.append(loss.item() / (num_fm + 1))
+        
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        # if i % 20 == 19 :
-        #     print('epoch = ', epoch + 1, ' step = ', i + 1, ' of total steps ', total_step, ' loss = ', round(loss.item(), 4))
-            
     train_loss = (sum(trn) / len(trn))
     train_loss_list.append(train_loss)
-    
+
     net.eval()
     val = []
     with torch.no_grad() :
@@ -122,33 +140,20 @@ for epoch in range(hyper_params['num_epochs']):
             # Forward pass
             outputs = net(images)
             loss = F.cross_entropy(outputs, labels)
-            val.append(loss)
+            val.append(loss.item())
 
-    val_loss = (sum(val) / len(val)).item()
+    val_loss = sum(val) / len(val)
     val_loss_list.append(val_loss)
+
     val_acc = _get_accuracy(data.valid_dl, net)
-    val_acc_list.append(val_acc)
+    
     experiment.log_metric("train_loss", train_loss)
     experiment.log_metric("val_loss", val_loss)
     experiment.log_metric("val_acc", val_acc * 100)
-    print('epoch : ', epoch + 1, ' / ', hyper_params['num_epochs'], ' | TL : ', round(train_loss, 4), ' | VL : ', round(val_loss, 4), ' | VA : ', round(val_acc * 100, 6))
-    
+
+    print('epoch : ', epoch + 1, ' / ', hyper_params["num_epochs"], ' | TL : ', round(train_loss, 6), ' | VL : ', round(val_loss, 6), ' | VA : ', round(val_acc * 100, 6))
+
     if (val_acc * 100) > min_val :
         print('saving model')
         min_val = val_acc * 100
         torch.save(net.state_dict(), savename)
-        
-# checking accuracy of best model
-# net.load_state_dict(torch.load(savename))
-# print('validation acc : ', _get_accuracy(data.valid_dl, net))
-# print('test acc : ', _get_accuracy(data.test_dl, net))
-
-# plt.plot(range(hyper_params['num_epochs']), train_loss_list, 'r', label = 'training_loss')
-# plt.plot(range(hyper_params['num_epochs']), val_loss_list, 'b', label = 'validation_loss')
-# plt.legend()
-# plt.savefig('../figures/' + str(hyper_params['dataset']) + '/less_data_medium_no_teacher/training_losses' + str(hyper_params['repeated']) + '.jpg')
-# plt.close()
-
-# plt.plot(range(hyper_params['num_epochs']), val_acc_list, 'r', label = 'validation_accuracy')
-# plt.legend()
-# plt.savefig('../figures/' + str(hyper_params['dataset']) + '/less_data_medium_no_teacher/validation_acc' + str(hyper_params['repeated']) + '.jpg')
--- code/train_less_data_no_teacher.sh
+++ code/train_less_data_no_teacher.sh
@@ -1,17 +1,74 @@
-python3 less_data_medium_no_teacher.py -m resnet10 -d imagenette -e 100
-python3 less_data_medium_no_teacher.py -m resnet14 -d imagenette -e 100
-python3 less_data_medium_no_teacher.py -m resnet18 -d imagenette -e 100
-python3 less_data_medium_no_teacher.py -m resnet20 -d imagenette -e 100
-python3 less_data_medium_no_teacher.py -m resnet26 -d imagenette -e 100
+python3 less_data_medium_no_teacher.py -m resnet10 -d imagenette -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d imagenette -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d imagenette -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d imagenette -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d imagenette -p 10 -e 100 -s 0
 
-python3 less_data_medium_no_teacher.py -m resnet10 -d imagewoof -e 100
-python3 less_data_medium_no_teacher.py -m resnet14 -d imagewoof -e 100
-python3 less_data_medium_no_teacher.py -m resnet18 -d imagewoof -e 100
-python3 less_data_medium_no_teacher.py -m resnet20 -d imagewoof -e 100
-python3 less_data_medium_no_teacher.py -m resnet26 -d imagewoof -e 100
+python3 less_data_medium_no_teacher.py -m resnet10 -d imagewoof -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d imagewoof -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d imagewoof -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d imagewoof -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d imagewoof -p 10 -e 100 -s 0
 
-python3 less_data_medium_no_teacher.py -m resnet10 -d cifar10 -e 100
-python3 less_data_medium_no_teacher.py -m resnet14 -d cifar10 -e 100
-python3 less_data_medium_no_teacher.py -m resnet18 -d cifar10 -e 100
-python3 less_data_medium_no_teacher.py -m resnet20 -d cifar10 -e 100
-python3 less_data_medium_no_teacher.py -m resnet26 -d cifar10 -e 100
+python3 less_data_medium_no_teacher.py -m resnet10 -d cifar10 -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d cifar10 -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d cifar10 -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d cifar10 -p 10 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d cifar10 -p 10 -e 100 -s 0
+
+
+python3 less_data_medium_no_teacher.py -m resnet10 -d imagenette -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d imagenette -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d imagenette -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d imagenette -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d imagenette -p 20 -e 100 -s 0
+
+python3 less_data_medium_no_teacher.py -m resnet10 -d imagewoof -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d imagewoof -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d imagewoof -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d imagewoof -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d imagewoof -p 20 -e 100 -s 0
+
+python3 less_data_medium_no_teacher.py -m resnet10 -d cifar10 -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d cifar10 -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d cifar10 -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d cifar10 -p 20 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d cifar10 -p 20 -e 100 -s 0
+
+
+python3 less_data_medium_no_teacher.py -m resnet10 -d imagenette -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d imagenette -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d imagenette -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d imagenette -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d imagenette -p 30 -e 100 -s 0
+
+python3 less_data_medium_no_teacher.py -m resnet10 -d imagewoof -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d imagewoof -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d imagewoof -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d imagewoof -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d imagewoof -p 30 -e 100 -s 0
+
+python3 less_data_medium_no_teacher.py -m resnet10 -d cifar10 -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d cifar10 -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d cifar10 -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d cifar10 -p 30 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d cifar10 -p 30 -e 100 -s 0
+
+
+python3 less_data_medium_no_teacher.py -m resnet10 -d imagenette -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d imagenette -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d imagenette -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d imagenette -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d imagenette -p 40 -e 100 -s 0
+
+python3 less_data_medium_no_teacher.py -m resnet10 -d imagewoof -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d imagewoof -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d imagewoof -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d imagewoof -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d imagewoof -p 40 -e 100 -s 0
+
+python3 less_data_medium_no_teacher.py -m resnet10 -d cifar10 -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet14 -d cifar10 -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet18 -d cifar10 -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet20 -d cifar10 -p 40 -e 100 -s 0
+python3 less_data_medium_no_teacher.py -m resnet26 -d cifar10 -p 40 -e 100 -s 0
