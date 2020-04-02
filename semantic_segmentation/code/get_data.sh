c821dc8072033868eb88d0c54465b5bcf2331a5b
akshay <akshayk.vnit@gmail.com>
2020-04-02 13:42:02 +0530

    removed files for restructuring

--- semantic_segmentation/code/get_data.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-git clone https://github.com/alexgkendall/SegNet-Tutorial.git
-mv SegNet-Tutorial data
--- semantic_segmentation/code/trad_kd.sh
+++
@@ -1,5 +0,0 @@
-python3 trad_kd.py -m resnet10 -e 50 -s 0
-python3 trad_kd.py -m resnet14 -e 50 -s 0
-python3 trad_kd.py -m resnet18 -e 50 -s 0
-python3 trad_kd.py -m resnet20 -e 50 -s 0
-python3 trad_kd.py -m resnet26 -e 50 -s 0
