2201e8130141d28011208263cf530349e8035f56
Akshay Kulkarni <akshayk.vnit@gmail.com>
2020-04-11 18:30:33 +0530

    fixed setup.sh

--- image_classification/scripts/setup.sh
+++ image_classification/scripts/setup.sh
@@ -15,9 +15,11 @@ python3 create_dataset.py -d cifar10 -p 20 -s 1
 python3 create_dataset.py -d cifar10 -p 30 -s 1
 python3 create_dataset.py -d cifar10 -p 40 -s 1
 
+mkdir -p ~/.fastai/data/imagenette/models
 cd ~/.fastai/data/imagenette/models
 wget https://www.dropbox.com/s/p8zeahm8f6ehkfy/resnet34_imagenette_bs64.pth
 
+mkdir -p ~/.fastai/data/imagewoof/models
 cd ~/.fastai/data/imagewoof/models
 wget https://www.dropbox.com/s/9xoj7d4qrlf94pc/resnet34_imagewoof_bs64.pth
 
